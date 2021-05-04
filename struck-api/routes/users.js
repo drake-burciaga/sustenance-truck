0d7b53d28eb39b49f842fa404e3cd8ea8af6d590
Drake Burciaga <drakeburciaga@gmail.com>
2021-05-04 08:57:37 -0500

    add mongodb routes

--- struck-api/routes/users.js
+++ struck-api/routes/users.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+// import users database from mongodb
+const usersModel = require('../models/users.model');
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
