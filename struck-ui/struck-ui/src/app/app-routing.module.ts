ff45f6c19d7f6b93b3e06032b0e918e3bc0e6bc2
Drake Burciaga <drakeburciaga@gmail.com>
2021-05-04 15:34:03 -0500

    add user routes

--- struck-ui/struck-ui/src/app/app-routing.module.ts
+++ struck-ui/struck-ui/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotComponent } from './auth/forgot/forgot.component';
+import { LoginComponent } from './auth/login/login.component';
+import { LogoutComponent } from './auth/logout/logout.component';
+import { NewUserComponent } from './auth/new-user/new-user.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // always available for everyone to see
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: NewUserComponent},
+  { path: 'forgot-password', component: ForgotComponent},
+  { path: 'logout', component: LogoutComponent},
+  //  lazy loading | only load when requested
+  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
+  // catch errors
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
