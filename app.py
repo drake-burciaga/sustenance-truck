d5bb64fdd7743431495f53facb8b9c6608c43e12
Drake Burciaga <drakeburciaga@gmail.com>
2021-05-04 23:11:18 -0500

    add login

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY']= '27795525de510704b1bdd2f0349a5e02'
 
 posts = [
     {
@@ -25,7 +28,19 @@ def home():
 # About Us 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='fucking tets')
+    return render_template('about.html', title='About')
+
+# Register User Form
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+# Login Form
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Welcome Back!', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
