beee9f0dd66012ec68321cc9dd1cfb9956b1e89e
Drake Burciaga <drakeburciaga@gmail.com>
2021-05-12 15:17:03 -0500

    add pagination

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
+from werkzeug.exceptions import RequestedRangeNotSatisfiable
 from app import app, db, bcrypt
 from app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from app.models import User, Post
@@ -12,8 +13,9 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route('/')
 @app.route('/home')
 def home():
-    # show all posts from database
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    # use paginate method | Set how many posts per page there will be
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 # About Us 
@@ -172,3 +174,13 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted.', 'success')
     return redirect (url_for('home'))
+
+# User Page
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
